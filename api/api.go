e8a18e2d40b8c2b2d63f397b367be1eaf4bc9846
xianlinbox <xianlinbox@gmail.com>
2024-07-05 21:19:55 -0500

    chore: Add login API endpoint and handler

--- api/api.go
+++ api/api.go
@@ -4,23 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
+	security "github.com/xianlinbox/simple_bank/api/security"
 	db "github.com/xianlinbox/simple_bank/db/sqlc"
 )
 
 type ApiServer struct {
 	store db.Store
 	router *gin.Engine
+	tokenMaker security.Maker
 }
 
-func NewServer(store db.Store) *ApiServer {	
+func NewServer(store db.Store, tokenMaker security.Maker) *ApiServer {	
+
 	router := gin.Default()
 	server := &ApiServer{
 		store: store,
+		tokenMaker: tokenMaker,
 	}
 	if engine,ok := binding.Validator.Engine().(*validator.Validate); ok {
 		engine.RegisterValidation("positiveAccountID", validateAccountID)
 	
 	router.POST("/users", server.CreateUser)}
+	router.POST("/user/login", server.Login)
 	router.POST("/accounts", server.CreateAccount)
 	router.GET("/accounts", server.ListAccounts)
 	router.GET("/accounts/:id", server.GetAccount)
--- api/user.go
+++ api/user.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/xianlinbox/simple_bank/db/sqlc"
@@ -14,7 +15,6 @@ type AddUserRequest struct {
 	FullName string `json:"FullName" binding:"required"`
 	Email    string `json:"Email" binding:"required" validate:"email"`
 }
-
 
 func  (server *ApiServer) CreateUser(c *gin.Context) {
 	var req AddUserRequest
@@ -40,4 +40,37 @@ func  (server *ApiServer) CreateUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+
+type LoginRequest struct {
+	Username string `json:"Username" binding:"required"`
+	Password string `json:"Password" binding:"required" validate:"min=8"`
+}
+
+func (server *ApiServer) Login(c *gin.Context) {
+	var req LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	user, err := server.store.GetUser(c, req.Username)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	err = util.CheckPassword(user.Password, req.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	newToken,err := server.tokenMaker.GenerateToken(user.Username, time.Minute*15)
+	if err != nil{
+		c.JSON(http.StatusUnauthorized, errorResponse(err))
+		return	
+	}
+	c.JSON(http.StatusOK, newToken)
 }

a789e271a1c5e643227065ddb1ce3943819b0778
xianlinbox <xianlinbox@gmail.com>
2024-07-19 14:51:51 -0500

    chore: Update Terraform and Go versions in CI workflow
    This commit updates the `ci.yaml` workflow in the `.github/workflows` directory to use the latest versions of Terraform and Go. The `TF_VERSION` environment variable is removed, and the `go-version` input for the `setup-go` action is updated to use the `$GO_VERSION` variable. This modification ensures that the CI workflow is using the most up-to-date dependencies and improves compatibility with the project.

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,11 +1,11 @@
 locals {
-    tags = {
-    owner       = "xianlinbox"
-    project    = "simple-bank"
+  tags = {
+    owner   = "xianlinbox"
+    project = "simple-bank"
   }
 }
 resource "aws_ecr_repository" "simple_bank_ecr_repo" {
-  name                 = "simple-bank-repo" 
+  name                 = "simple-bank-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {

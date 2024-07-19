c4262600ee3bf21433794492c425041cc4a9b221
xianlinbox <xianlinbox@gmail.com>
2024-07-18 22:40:00 -0500

    chore: Update Terraform backend configuration for S3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -7,14 +7,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
-    region = "${var.region}"
+    bucket = "xianlinbox-simple-bank"
+    key    = "tf-state"
+    region = "us-east-1"
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "${var.region}"
   profile = "tf-user"
 }

306077f5280727de90cbefbd8c89938987691955
xianlinbox <xianlinbox@gmail.com>
2024-07-21 18:20:10 -0500

    chore: Update RDS configuration in infra
    The `rds.tf` file in the `infra` directory has been modified to update the configuration of the RDS instance. The `allocated_storage` parameter has been changed to 10, the `db_name` has been set to "simple-bank", and the `engine_version` has been updated to "16.0". Additionally, the `skip_final_snapshot` parameter has been set to true. This change ensures that the RDS instance is properly configured for the application's needs.

--- infra/rds.tf
+++ infra/rds.tf
@@ -8,12 +8,12 @@ data "aws_secretsmanager_secret_version" "password" {
 }
 
 resource "aws_db_instance" "simple_bank_db" {
-  allocated_storage    = 10
-  db_name              = "simple-bank"
-  engine               = "postgres"
-  engine_version       = "16.0"
-  instance_class       = "db.t3.micro"
-  username             = "root"
-  password             = data.aws_secretsmanager_secret_version.password
-  skip_final_snapshot  = true
+  allocated_storage   = 10
+  db_name             = "simple-bank"
+  engine              = "postgres"
+  engine_version      = "16.0"
+  instance_class      = "db.t3.micro"
+  username            = "root"
+  password            = data.aws_secretsmanager_secret_version.password
+  skip_final_snapshot = true
 }
--- infra/secrets.tf
+++ infra/secrets.tf
@@ -3,6 +3,6 @@ resource "aws_secretsmanager_secret" "simple-bank-db-password" {
 }
 
 resource "aws_secretsmanager_secret_version" "simple-bank-db-password" {
-  secret_id = aws_secretsmanager_secret.simple-bank-db-password.id
+  secret_id     = aws_secretsmanager_secret.simple-bank-db-password.id
   secret_string = var.DB_PASSWORD
 }
